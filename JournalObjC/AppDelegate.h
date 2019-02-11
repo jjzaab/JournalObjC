afcbc98f25e8f14323ac9787de76ea1e1552706b
XMS_JZhan <xms_jzhan@XMS-JZhan.local>
2019-02-11 13:04:35 -0700

    Initial Commit

---
+++ JournalObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JournalObjC
+//
+//  Created by XMS_JZhan on 2/11/19.
+//  Copyright © 2019 XMS_JZhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JournalObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JournalObjC
+//
+//  Created by XMS_JZhan on 2/11/19.
+//  Copyright © 2019 XMS_JZhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
